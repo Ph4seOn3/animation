b229642bbfda127fe2c4b5a2fcbf61f28812b2da
Jeff Korzeniowski <Ph4seon3@proton.me>
2022-12-12 10:27:23 -0500

    Initial Commit

---
+++ animation/animationApp.swift
@@ -0,0 +1,17 @@
+//
+//  animationApp.swift
+//  animation
+//
+//  Created by Jeff Korzeniowski on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct animationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
